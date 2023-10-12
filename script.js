7a71bb9ba40d18b3b1837e535631d0f5b4eb94fc
achorn <joshuaachorn@gmail.com>
2023-10-11 21:26:25 -0700

    scaling entire game

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var text = ["LOADING.  ", "LOADING.. ", "LOADING...", "LOADING"];
-var counter = 0;
-var loadingElem = document.getElementById("loadingDisplay");
-var cryButton = document.getElementsByClassName("cryButton")[0];
+let root = document.documentElement;
+let width = document.getElementById("width");
+let height = document.getElementById("height");
+window.addEventListener("resize", scale);
 
-var sadButton = document.getElementsByClassName("sadButton")[0];
-var errorButtons = document.getElementsByClassName("error");
-var inst = setInterval(change, 1000);
+function scale() {
+  let size = Math.min(window.innerWidth, window.innerHeight);
+  root.style.setProperty("--size", size + "px");
+  // root.style.setProperty("--font_one", size / 30 + "px");
+  // root.style.setProperty("--border-thickness", Math.floor(size / 65) + "px");
+}
+
+scale();
