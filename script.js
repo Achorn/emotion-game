9ef993b2e2f399aa4d857d57439e5b7c52ade2e8
achorn <joshuaachorn@gmail.com>
2023-10-13 13:54:22 -0700

    adding beginning game mechanics

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let soundButton = document.getElementById("sound");
 let volOffIcon = document.getElementById("soundOff");
 let volOnIcon = document.getElementById("soundOn");
 let startButton = document.getElementById("startButton");
+let textContainer = document.getElementById("text-container");
 
 window.addEventListener("resize", scale);
 function scale() {
@@ -25,27 +26,54 @@ soundButton.addEventListener("click", () => {
   let muted = ThemeSong.muted;
   volOffIcon.style.display = muted ? "none" : "inline";
   volOnIcon.style.display = muted ? "inline" : "none";
-
   ThemeSong.muted = !ThemeSong.muted;
 });
 
 //startScreen
 startButton.addEventListener("click", () => {
-  ThemeSong.play();
-
   kid_laughing_audio.currentTime = 0;
   kid_laughing_audio.play();
+  startGame();
 });
 
 //chapters
 
-let chapters = {
-  1: { title: "Your Time", content: "" },
-  2: { title: "Going Out", content: "" },
-  3: { title: "A Release", content: "" },
-  4: { title: "Let it In!", content: "" },
-  5: { title: "Let it Out!", content: "" },
-  6: { title: "The Great Outdoors", content: "" },
-  7: { title: "Cut it off", content: "" },
-  8: { title: "A Familiar Feeling", content: "" },
-};
+let chapters = [
+  { id: 1, title: "Your Time", content: "" },
+  { id: 2, title: "Going Out", content: "" },
+  { id: 3, title: "A Release", content: "" },
+  { id: 4, title: "Let it In!", content: "" },
+  { id: 5, title: "Let it Out!", content: "" },
+  { id: 6, title: "The Great Outdoors", content: "" },
+  { id: 7, title: "Cut it off", content: "" },
+  { id: 8, title: "A Familiar Feeling", content: "" },
+];
+function startGame() {
+  ThemeSong.play();
+  startChapter(1);
+}
+function startChapter(chapterId) {
+  const chapter = chapters.find((chapter) => chapter.id === chapterId);
+  console.log(chapter);
+
+  ShowChapterDescription(chapter);
+  // play game
+  // move on to next game
+}
+
+function ShowChapterDescription(chapter) {
+  textContainer.innerHTML = "";
+  //chapter
+  let chapterNum = document.createElement("h2");
+  chapterNum.innerHTML = `Chapter: ${chapter.id}`;
+  textContainer.appendChild(chapterNum);
+
+  let chapterTitle = document.createElement("h1");
+  chapterTitle.innerHTML = chapter.title;
+  textContainer.appendChild(chapterTitle);
+  let beginChapterButton = document.createElement("button");
+  beginChapterButton.innerHTML = "Begin";
+  textContainer.appendChild(beginChapterButton);
+  // title
+  // button to play game
+}
